d7211601dff8267623e19d3ba4dc63efc9df641a
phani <me@example.com>
2020-10-03 10:05:36 +0530

    login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,87 +2,76 @@
 
 <head>
 
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
-  <meta name="description" content="" />
-  <meta name="author" content="" />
-  <title>login form</title>
-
-  <!-- CSS only -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-  <link rel="stylesheet" href="/css/app.css">
-
-  <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
-
-
-
-
-  <!-- JS, Popper.js, and jQuery -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-    crossorigin="anonymous"></script>
-  <script src="https://kit.fontawesome.com/4e73ca9112.js" crossorigin="anonymous"></script>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>login form</title>
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/app.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    </script>
+    <script src="https://kit.fontawesome.com/4e73ca9112.js" crossorigin="anonymous"></script>
 </head>
 
 <body class="login-page">
-  <div class="formbox">
+    <div class="formbox">
+        <form>
+            <svg id="svg3" height="1" width="1">
+                <line x1="0" y1="0" x2="0" y2="0" style="stroke:rgb(236, 235, 235);stroke-width:1" />
+            </svg>
+            <div class="logo">
+                <img src="/images/Logo.png" alt="logo icon" height="100px" width="100px">
+            </div>
+            <div class="form-group">
+                <label>Username</label>
+                <div class="position-relative has-icon-left">
+                    <input type="text" id="username" class="form-control input-shadow">
+                    <div class="form-control-position">
+                        <img src="/images/username.svg">
+                    </div>
+                </div>
+            </div>
 
-    <form>
-      <svg id="svg3" height="1" width="1">
-        <line x1="0" y1="0" x2="0" y2="0" style="stroke:rgb(236, 235, 235);stroke-width:1" />
-      </svg>
-      <div id="logo">
-        <img src="/images/Logo.png" alt="logo icon" height="100px" width="100px">
-      </div>
-      <div class="form-group">
-        <label>Username</label>
-        <div class="position-relative has-icon-left">
-          <input type="text" id="username" class="form-control input-shadow">
-          <div class="form-control-position">
-            <img src="/images/username.svg">
+            <div class="form-group">
+                <label>Password</label>
+                <div class="position-relative has-icon-left">
+                    <input type="password" id="password" class="form-control input-shadow">
+                    <div class="form-control-position">
+                        <img src="/images/password.svg">
+                    </div>
+                </div>
+            </div>
 
-          </div>
-        </div>
-      </div>
+            <button type="button" id="button" class="btn btn-outline-primary">
+                <div class="login">LOGIN</div>
+            </button><br>
 
-      <div class="form-group">
-        <label>Password</label>
-        <div class="position-relative has-icon-left">
-          <input type="password" id="password" class="form-control input-shadow">
-          <div class="form-control-position">
-            <img src="/images/password.svg">
-          </div>
-        </div>
-      </div>
+            <a href="#"><img src="/images/forgot 1.svg" class="retrieve"></a>
+            <a href="#" class="retrieves"> Forgot Username / Password</a><br>
+            <a href="#" class="fas fa-plus-circle" id="new"></a>
+            <a href="#" class="news">Create New Account</a>
 
-
-      <button type="button" id="button" class="btn btn-outline-primary">
-        <div id="login">LOGIN</div>
-      </button><br>
-      <a href="#"><img id="retrieve" src="/images/forgot 1.svg"></a> <a href="#" id="retrieves"> Forgot Username /
-        Password</a><br>
-      <a href="#" class="fas fa-plus-circle" id="new"></a><a href="#" id="news">Create New Account</a>
-
-      <svg id="svg1" height="63" width="42">
-        <line x1="0" y1="0" x2="130" y2="70" style="stroke:rgb(236, 235, 235);stroke-width:1" />
-      </svg>
-      <svg id="svg2" height="39" width="80">
-        <line x1="0" y1="0" x2="130" y2="70" style="stroke:rgb(236, 235, 235);stroke-width:1" />
-      </svg>
-
-
-    </form>
-
-
-
+            <svg class="svg1" height="59" width="42">
+                <line x1="0" y1="0" x2="130" y2="70" style="stroke:rgb(236, 235, 235);stroke-width:1" />
+            </svg>
+            <svg class="svg2" height="38" width="80">
+                <line x1="0" y1="0" x2="130" y2="70" style="stroke:rgb(236, 235, 235);stroke-width:1" />
+            </svg>
+        </form>
+    </div>
 </bodY>
 
 </html>
